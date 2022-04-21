d7d3576b0d9974f532191ed8321fe5fd5bd5979a
Kacper Mazur <kacper5103@gmail.com>
2022-04-22 00:44:23 +0100

    API + dropdown

--- watch_show.php
+++ watch_show.php
@@ -21,16 +21,16 @@ if (isset($_GET['episode'])) $episode = $_GET['episode'];
 
 $row = mysqli_fetch_array($r, MYSQLI_ASSOC);
 
-echo '
-    <title>Webflix</title>
-    <div id="mv-info">
-    <div id="content-embed">
-    <iframe class="embed-responsive-item" src=' . $row['link'] . '&s=' . $season . '&e=' . $episode . ' 
-    frameborder="no" scrolling="no" allow="autoplay; encrypted-media" allowfullscreen="yes" style="width: 100%; height: 100%;"></iframe></div>
-    </iframe>
-    </div>
-    </div>
-        ';
+// echo '
+//     <title>Webflix</title>
+//     <div id="mv-info">
+//     <div id="content-embed">
+//     <iframe class="embed-responsive-item" src=' . $row['link'] . '&s=' . $season . '&e=' . $episode . ' 
+//     frameborder="no" scrolling="no" allow="autoplay; encrypted-media" allowfullscreen="yes" style="width: 100%; height: 100%;"></iframe></div>
+//     </iframe>
+//     </div>
+//     </div>
+//         ';
 
 
 # Extract show ID
@@ -50,17 +50,23 @@ $previous_episode = $episode - 1;
 $previous_season = $season - 1;
 
 # Buttons for navigating through seasons and episodes
-echo '<a href="watch_show.php?id=' . $row['id'] . '&season=' . $season . '&episode=' . $previous_episode . '"> <button id="previousEpisode" type="button" class="btn btn-secondary" role="button"><h3><i class="fa-solid fa-caret-left"></i> Previous episode</h3></button></a>';
 echo '<a href="watch_show.php?id=' . $row['id'] . '&season=' . $season . '&episode=' . ++$episode . '"> <button id="nextEpisode" type="button" class="btn btn-secondary" role="button"><h3><i class="fa-solid fa-caret-right"></i> Next episode</h3></button></a>';
-echo '<a href="watch_show.php?id=' . $row['id'] . '&season=' . $previous_season . '&episode=1"> <button id="previousSeason" type="button" class="btn btn-secondary" role="button"><h3><i class="fa-solid fa-caret-left"></i> Previous season</h3></button></a>';
 echo '<a href="watch_show.php?id=' . $row['id'] . '&season=' . ++$season . '&episode=1"> <button id="nextSeason" type="button" class="btn btn-secondary" role="button"><h3><i class="fa-solid fa-caret-right"></i> Next Season</h3></button></a>';
 
 ?>
+<select id="season" name="season" onchange="location = this.value;">
+  <option value="" disabled selected>Select season</option>
+</select>
+
+<select id="episode" name="episode" onchange="location = this.value;">
+  <option value="" disabled selected>Select episode</option>
+</select>
 
 <script>
   var showID = '<?= $showID ?>';
   var episode = '<?= $episode ?>' - 1;
   var season = '<?= $season ?>' - 1;
+  var id = '<?= $row['id'] ?>';
 
   const season_url = '/season/';
   const base_url = 'https://api.themoviedb.org/3/tv/'
@@ -70,58 +76,69 @@ echo '<a href="watch_show.php?id=' . $row['id'] . '&season=' . ++$season . '&epi
   const api_url2 = base_url + showID + api_key;
 
   // Get number of epsiodes in season
-  function getEpisodes(api_url) {
+  function getEpisodesAndSeasons(api_url, api_url2) {
     fetch(api_url)
       .then(response => response.json())
       .then(data => {
         checkEpisode(data.episodes.length);
+        displayEpisodes(data.episodes.length);
       })
-  }
 
-  // Get number of seasons in show
-  function getSeasons(api_url2) {
     fetch(api_url2)
       .then(response2 => response2.json())
       .then(data2 => {
         checkSeason(data2.number_of_seasons);
+        displaySeasons(data2.number_of_seasons);
       })
   }
 
-  console.log(episode);
+  function displaySeasons(seasons) {
+    var select = document.getElementById("season");
+    for (var i = 1; i <= seasons; i++) {
+      var option = document.createElement('option');
+      option.text = "Season " + i;
+      option.value = "watch_show.php?id=" + id + "&season=" + i + '&episode=1';
+      select.add(option);
+    }
+  }
+
+  function displayEpisodes(episodes) {
+    var select = document.getElementById("episode");
+    for (var i = 1; i <= episodes; i++) {
+      var option = document.createElement('option');
+      option.text = "Episode " + i;
+      option.value = "watch_show.php?id=" + id + "&season=" + season + '&episode=' + i;
+      select.add(option);
+    }
+  }
   // Display buttons appropriately to number of episodes in a season
   function checkEpisode(episodeNo) {
-    if (episode < episodeNo) {
-      document.getElementById("previousEpisode").style.display = "inline";
-      document.getElementById("nextEpisode").style.display = "inline";
-    }
     if (episode == episodeNo || episode > episodeNo) {
-      document.getElementById("previousEpisode").style.display = "inline";
       document.getElementById("nextEpisode").style.display = "none";
     }
-    if (episode == 1 || episode == 0) {
-      document.getElementById("previousEpisode").style.display = "none";
+    if(episode < episodeNo){
+      document.getElementById("nextSeason").style.display = "none";
     }
   }
 
   // Display buttons appropriately to number of seasons in a show
-  function checkSeason(seasonNo) {
-    if (season < seasonNo) {
-      console.log(season);
+  function checkSeason(seasonNo, episodeNo) {
+    if (season < seasonNo && episode == episodeNo) {
       document.getElementById("nextSeason").style.display = "inline";
-      document.getElementById("previousSeason").style.display = "inline";
-
     }
     if (season == seasonNo || season > seasonNo) {
       document.getElementById("nextSeason").style.display = "none";
-      document.getElementById("previousSeason").style.display = "inline";
     }
-    if (season == 1 || season == 0) {
-      document.getElementById("previousSeason").style.display = "none";
+    if (season == seasonNo && episode == episodeNo) {
+      document.getElementById("nextSeason").style.display = "none";
+    }
+    if((season > seasonNo) && (episode > episodeNo)){
+      
     }
   }
 
-  getEpisodes(api_url);
-  getSeasons(api_url2);
+  getEpisodesAndSeasons(api_url, api_url2);
+
 </script>
 
 </html>
